7792eb7140dbde2a1299afd525059d26e82907c2
lsr <imlimr1232@gmail.com>
2020-12-26 15:47:58 +0900

    Handling ROutes with Express

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const app = express();
 
 const PORT = 4000;
 
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
+  console.log(`Listening on: wwhttp://localhost:${PORT}`);
 }
 
 app.listen(PORT, handleListening);
